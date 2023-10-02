c40c39fc69cee9b0615c8532c9bc98a236bac145
bluemagick1 <146766757+bluemagick1@users.noreply.github.com>
2023-10-02 13:35:24 -0600

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -15,7 +15,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x8e8d2f8801a5Cb389eEF7931389639d60B969e21/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
